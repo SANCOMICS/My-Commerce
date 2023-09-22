aa865219aca6b01a9cb6cfc45a730360146c51d0
Mbiybee Prince-Charles Berinyuy <mbiyber@outlook.com>
2023-09-22 13:23:33 +0100

    Home body
    starting home body

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import{Link} from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <>
+      <section className='home-wrapper-1 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner position-relative p-3'>
+                <img src='images/main-banner-1.jpg' 
+                  className='image-fluid rounded-3 mainbPic'
+                alt='main banner' />
+                <div className='main-banner-content position-absolute'>
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.00 or $41.62/mo</p>
+                  <Link className='button' >BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+
+      </section>
+    </>
   )
 }
 
