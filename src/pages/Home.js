dcc31cdb25fa35de9d051da9c098d63aef2e27aa
Mbiybee Prince-Charles Berinyuy <mbiyber@outlook.com>
2023-09-23 17:35:37 +0100

    next
    commit of first banner in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,11 @@ import{Link} from 'react-router-dom'
 const Home = () => {
   return (
     <>
-      <section className='home-wrapper-1 py-5'>
+      <section className='home-wrapper-1 py-4'>
         <div className='container-xxl'>
-          <div className='row'>
+          <div className='row px-3'>
             <div className='col-6'>
-              <div className='main-banner position-relative p-3'>
+              <div className='small-banner position-relative p-1'>
                 <img src='images/main-banner-1.jpg' 
                   className='image-fluid rounded-3 mainbPic'
                 alt='main banner' />
@@ -19,6 +19,54 @@ const Home = () => {
                   <Link className='button' >BUY NOW</Link>
                 </div>
               </div>
+            </div>
+            <div className='col-6'>
+             <div className='d-flex flex-wrap justify-content-between align-items-center'>
+             <div className='main-banner position-relative p-1'>
+                <img src='images/catbanner-01.jpg' 
+                  className='image-fluid rounded-3 mainb'
+                alt='main banner' />
+                <div className='small-banner-content position-absolute'>
+                  <h4>BEST SALE</h4>
+                  <h5>Laptops Max</h5>
+                  <p>From $1699.00 or<br/> $64.62/mo</p>
+                  
+                </div>
+              </div>
+              <div className='main-banner position-relative p-1'>
+                <img src='images/catbanner-03.jpg' 
+                  className='image-fluid rounded-3 mainb'
+                alt='main banner' />
+                <div className='small-banner-content position-absolute'>
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>Buy IPad Air</h5>
+                  <p>From $599.00 or<br/> $49.91/mo for 12 mo. *</p>
+                  
+                </div>
+              </div>
+              <div className='main-banner position-relative p-1'>
+                <img src='images/catbanner-02.jpg' 
+                  className='image-fluid rounded-3 mainb'
+                alt='main banner' />
+                <div className='small-banner-content position-absolute'>
+                  <h4>15% OFF</h4>
+                  <h5>Smartwatch 7</h5>
+                  <p>Shop the latest band<br/> styles and colors.</p>
+
+                </div>
+              </div>
+              <div className='main-banner position-relative p-1'>
+                <img src='images/catbanner-04.jpg' 
+                  className='image-fluid rounded-3 mainb'
+                alt='main banner' />
+                <div className='small-banner-content position-absolute'>
+                  <h4>Free Engraving</h4>
+                  <h5>AirPods Max</h5>
+                  <p>High-fidelity playback &<br/> ultra-low distortion</p>
+                  
+                </div>
+              </div>
+             </div>
             </div>
           </div>
         </div>
